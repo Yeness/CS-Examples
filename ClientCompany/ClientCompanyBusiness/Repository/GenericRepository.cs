c7ecb4a5264ac455e25300c01513442ee9be2bdd
Yusuf Elmas <yenes_656@hotmail.com>
2024-05-04 02:25:15 +0300

    Generic Repository Added

---
+++ ClientCompany/ClientCompanyBusiness/Repository/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using ClientCompanyCore.BaseEntity;
+
+namespace ClientCompanyBusiness.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly AddDbContext _dbContext;
+
+        public GenericRepository(AddDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbContext.GetAll<TEntity>().AsNoTracking();
+        }
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbContext.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
+        }
+        public async Task Create(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task Update(int id, TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+  
+}
---
+++ ClientCompany/ClientCompanyBusiness/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClientCompanyCore.BaseEntity;
+
+namespace ClientCompanyBusiness.Repository
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> GetById(int id);
+        Task Create(TEntity entity);
+        Task Update(int id, TEntity entity);
+        Task Delete(int id);
+    }
+}
